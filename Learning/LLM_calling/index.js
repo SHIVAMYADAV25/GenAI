693cf78e95f339b11ed2d834e5e7005c73a5660a
Shivam Yadav <shivamy121314@gmail.com>
2025-12-27 01:14:16 +0530

    streamed output in LLM

--- Learning/LLM_calling/index.js
+++ Learning/LLM_calling/index.js
@@ -2,17 +2,33 @@ import OpenAI from "openai";
 
 const OPENAI_API_KEY = "";
 
+// const client = new OpenAI({
+//     apiKey : OPENAI_API_KEY
+// })
+
+// const response = await client.response.create({
+//     model : "gpt-4.1",
+//     input: "Write a story about code in AI world",
+//     temperature : 0.7,
+//     max_output_tokens: 250
+// })
+
+// console.log(response)
+
 const client = new OpenAI({
     apiKey : OPENAI_API_KEY
 })
 
-const response = await client.response.create({
+const stream = await client.response.create({
     model : "gpt-4.1",
     input: "Write a story about code in AI world",
     temperature : 0.7,
-    max_output_tokens: 250
+    max_output_tokens: 250,
+    stream : true
 })
 
-console.log(response)
+for(const event of stream){
+    process.stdout.write(event.delta)
+}
 
 
