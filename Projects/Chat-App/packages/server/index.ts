20059b6965d60def9ab6cb228d269225618b1d27
Shivam Yadav <shivamy121314@gmail.com>
2025-12-27 18:28:00 +0530

    memory management in chat-bot

--- Projects/Chat-App/packages/server/index.ts
+++ Projects/Chat-App/packages/server/index.ts
@@ -11,16 +11,24 @@ const app = express();
 app.use(express.json());
 const port = process.env.PORT || 3000;
 
+// let lastResponseId : string | null = ""
+//conversationId -> lastResponseId
+// conv1 -> 100
+// conv2 -> 200
+const conversations = new Map<string,string>();
 
 app.post("/api/chat", async (req: Request, res: Response) => {
-    const { prompt } = req.body;
+    const { prompt , conversationId } = req.body;
 
     const response = await client.responses.create({
         model : "gpt-4o-mini",
         input : prompt,
         temperature : 0.2,
         max_output_tokens : 150,
+        previous_response_id : conversations.get(conversationId)
     })
+
+    conversations.set(conversationId,response.id);
 
     res.json({
         message : response.output_text
