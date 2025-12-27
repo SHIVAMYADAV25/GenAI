7b205a5c6532d7e2a2485b5c30a11b7a66ca9bd7
Shivam Yadav <shivamy121314@gmail.com>
2025-12-27 18:44:06 +0530

    Error handling

--- Projects/Chat-App/packages/server/index.ts
+++ Projects/Chat-App/packages/server/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import type { Request, Response } from "express";
 import "dotenv/config";
 import OpenAI from "openai";
+import z from "zod"
 
 const client = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
@@ -16,23 +17,41 @@ const port = process.env.PORT || 3000;
 // conv1 -> 100
 // conv2 -> 200
 const conversations = new Map<string,string>();
+const chatSchema = z.object({
+  prompt : z.string()
+  .trim()
+  .min(1, "Prompt is required")
+  .max(100,"Prompt is too long (max 100 characters)"),
+  conversationId : z.string()
+  .uuid() 
+})
 
 app.post("/api/chat", async (req: Request, res: Response) => {
-    const { prompt , conversationId } = req.body;
+  const parseResult = chatSchema.safeParse(req.body);
 
-    const response = await client.responses.create({
-        model : "gpt-4o-mini",
-        input : prompt,
-        temperature : 0.2,
-        max_output_tokens : 150,
-        previous_response_id : conversations.get(conversationId)
-    })
-
-    conversations.set(conversationId,response.id);
-
-    res.json({
-        message : response.output_text
-    })
+  if(!parseResult.success){
+    res.status(400).json(parseResult.error.format());
+    return;
+  }try {
+    
+      const { prompt , conversationId } = req.body;
+  
+      const response = await client.responses.create({
+          model : "gpt-4o-mini",
+          input : prompt,
+          temperature : 0.2,
+          max_output_tokens : 150,
+          previous_response_id : conversations.get(conversationId)
+      })
+  
+      conversations.set(conversationId,response.id);
+  
+      res.json({
+          message : response.output_text
+      })
+  } catch (error) {
+    res.status(500).json({error : "Failed to generate a response"})
+  }
 });
 
 app.get("/", (req: Request, res: Response) => {
