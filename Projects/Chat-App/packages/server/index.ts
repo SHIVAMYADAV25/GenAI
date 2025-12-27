dbc828840cec194d1d2977e58ede85afb43cf166
Shivam Yadav <shivamy121314@gmail.com>
2025-12-27 18:04:22 +0530

    prettier changes

--- Projects/Chat-App/packages/server/index.ts
+++ Projects/Chat-App/packages/server/index.ts
@@ -1,15 +1,15 @@
-import express from "express"
-import type { Request ,Response } from "express";
-import "dotenv/config"
+import express from "express";
+import type { Request, Response } from "express";
+import "dotenv/config";
 
 const app = express();
 
-const port = process.env.PORT ||3000;
+const port = process.env.PORT || 3000;
 
-app.get("/",(req : Request,res:Response)=>{
-    res.send(process.env.OPENAI_API_KEY);
-})
+app.get("/", (req: Request, res: Response) => {
+  res.send(process.env.OPENAI_API_KEY);
+});
 
-app.listen(port,()=>{
-    console.log(`Server is running on http://localhost:${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
