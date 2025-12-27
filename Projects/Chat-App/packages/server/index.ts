632e0dab534cf608e43fc4deab4763438d3e5d34
Shivam Yadav <shivamy121314@gmail.com>
2025-12-27 18:27:22 +0530

    LLM API end point route

--- Projects/Chat-App/packages/server/index.ts
+++ Projects/Chat-App/packages/server/index.ts
@@ -1,15 +1,36 @@
-import express from "express"
-import type { Request ,Response } from "express";
-import "dotenv/config"
+import express from "express";
+import type { Request, Response } from "express";
+import "dotenv/config";
+import OpenAI from "openai";
+
+const client = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
 
 const app = express();
+app.use(express.json());
+const port = process.env.PORT || 3000;
 
-const port = process.env.PORT ||3000;
 
-app.get("/",(req : Request,res:Response)=>{
-    res.send(process.env.OPENAI_API_KEY);
-})
+app.post("/api/chat", async (req: Request, res: Response) => {
+    const { prompt } = req.body;
 
-app.listen(port,()=>{
-    console.log(`Server is running on http://localhost:${port}`);
-})
+    const response = await client.responses.create({
+        model : "gpt-4o-mini",
+        input : prompt,
+        temperature : 0.2,
+        max_output_tokens : 150,
+    })
+
+    res.json({
+        message : response.output_text
+    })
+});
+
+app.get("/", (req: Request, res: Response) => {
+  res.send(process.env.OPENAI_API_KEY);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
